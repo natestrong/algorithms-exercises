73c016f80d68cae35b6d219a899cb6075de8ede1
nstrong <nathanstrong.fx@gmail.com>
2021-07-21 16:57:17 -0700

    bubble sort

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -2,22 +2,31 @@
   Write a bubble sort here
   Name the function bubbleSort
   Return the sorted array at the end
-  
+
   To run the tests, change the `test.skip(…)` below to `test(…)`
-  
+
   Bubble sort works by comparing two adjacent numbers next to each other and then
   swapping their places if the smaller index's value is larger than the larger
   index's. Continue looping through until all values are in ascending order
 */
 
 function bubbleSort(nums) {
-  // code goes here
+    for (let i = 0; i < nums.length; i++) {
+        for (let k = i + 1; k < nums.length; k++) {
+            if (nums[i] > nums[k]) {
+                const temp = nums[k];
+                nums[k] = nums[i];
+                nums[i] = temp;
+            }
+        }
+    }
+    return nums;
 }
 
 // unit tests
 // do not modify the below code
-test.skip("bubble sort", function () {
-  const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
-  const sortedNums = bubbleSort(nums);
-  expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+test("bubble sort", function () {
+    const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
+    const sortedNums = bubbleSort(nums);
+    expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 });
