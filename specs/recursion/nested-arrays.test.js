f6af56f6f718d8b84b8146b84a06097988eea1ce
nstrong <nathanstrong.fx@gmail.com>
2021-07-22 12:17:54 -0700

    insertion sort

--- specs/recursion/nested-arrays.test.js
+++ specs/recursion/nested-arrays.test.js
@@ -1,21 +1,26 @@
-/* 
-
+/*
   write a function that accepts an array that only contains
   two types of things: numbers and arrays. those nested arrays
   also only contain numbers and other, nested arrays.
 
   example: nestedAdd([1, 2, [3]]) = 6
            nestedAdd([[[2]], 1, [1, 3]]) = 7
- 
  */
-
 function nestedAdd(array) {
-  // write code here
+    let result = 0;
+    for (const numOrArray of array) {
+        if (Array.isArray(numOrArray)) {
+            result += nestedAdd(numOrArray);
+        } else {
+            result += numOrArray;
+        }
+    }
+    return result;
 }
 
-test.skip("nested arrays addition", () => {
-  expect(nestedAdd([1, 2, 3])).toEqual(6);
-  expect(nestedAdd([1, [2], 3])).toEqual(6);
-  expect(nestedAdd([[[[[[[[[5]]]]]]]]])).toEqual(5);
-  expect(nestedAdd([10, [12, 14, [1], [16, [20]]], 10, 11])).toEqual(94);
+test("nested arrays addition", () => {
+    expect(nestedAdd([1, 2, 3])).toEqual(6);
+    expect(nestedAdd([1, [2], 3])).toEqual(6);
+    expect(nestedAdd([[[[[[[[[5]]]]]]]]])).toEqual(5);
+    expect(nestedAdd([10, [12, 14, [1], [16, [20]]], 10, 11])).toEqual(94);
 });
