54bd8468f320a358ca2e0bed66793f81d479e2fd
nstrong <nathanstrong.fx@gmail.com>
2021-07-22 14:50:16 -0700

    insertion sort

--- specs/quick-sort/quick-sort.test.js
+++ specs/quick-sort/quick-sort.test.js
@@ -11,16 +11,32 @@
   is called on a list with length less-than-or-equal-to 1. In the base case, just return the array given.
 
 */
-
 function quickSort(nums) {
-  // code goes here
+    if (nums.length <= 1) return nums;
+
+    const pivot = Math.floor(nums.length / 2);
+    const left = [];
+    const right = [];
+    const center = [];
+    for (const num of nums) {
+        if (num === nums[pivot]) center.push(num);
+        else if (num < nums[pivot]) left.push(num);
+        else right.push(num);
+    }
+
+    const leftSorted = quickSort(left);
+    const rightSorted = quickSort(right);
+
+    return leftSorted.concat(center, rightSorted);
 }
+
+// console.log(quickSort([10, 3, 6, 8, 2, 1, 8, 9, 11, 4, 16, 5]));
 
 // unit tests
 // do not modify the below code
-test.skip("quickSort", function () {
-  const input = [10, 8, 2, 1, 6, 3, 9, 4, 7, 5];
-  const answer = quickSort(input);
+test("quickSort", function () {
+    const input = [10, 8, 2, 1, 6, 3, 9, 4, 7, 5];
+    const answer = quickSort(input);
 
-  expect(answer).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    expect(answer).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 });
