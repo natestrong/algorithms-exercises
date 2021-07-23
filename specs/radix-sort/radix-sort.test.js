4f5dd505a7b2809db39b2fdf636d34faed791f20
nstrong <nathanstrong.fx@gmail.com>
2021-07-22 17:40:15 -0700

    insertion sort

--- specs/radix-sort/radix-sort.test.js
+++ specs/radix-sort/radix-sort.test.js
@@ -8,69 +8,85 @@
   it ends up being a lot more simple to implement.
 
 */
-
 function radixSort(array) {
-  // code goes here
+    // find longest number
+
+    // create how many buckets are needed
+    // an array of 10 arrays
+
+    // for loop for how many iterations
+        // enqueue the numbers into their buckets
+
+        // for loop for each bucket
+        // dequeue all of the results
 }
 
-// unit tests
-// do not modify the below code
-describe.skip("radix sort", function () {
-  it("should sort correctly", () => {
-    const nums = [
-      20,
-      51,
-      3,
-      801,
-      415,
-      62,
-      4,
-      17,
-      19,
-      11,
-      1,
-      100,
-      1244,
-      104,
-      944,
-      854,
-      34,
-      3000,
-      3001,
-      1200,
-      633
-    ];
-    const ans = radixSort(nums);
-    expect(ans).toEqual([
-      1,
-      3,
-      4,
-      11,
-      17,
-      19,
-      20,
-      34,
-      51,
-      62,
-      100,
-      104,
-      415,
-      633,
-      801,
-      854,
-      944,
-      1200,
-      1244,
-      3000,
-      3001
-    ]);
-  });
-  it("should sort 99 random numbers correctly", () => {
-    const fill = 99;
-    const nums = new Array(fill)
-      .fill()
-      .map(() => Math.floor(Math.random() * 500000));
-    const ans = radixSort(nums);
-    expect(ans).toEqual(nums.sort());
-  });
-});
+function getDigit(number, place, longestNumber) {
+    const string = number.toString();
+    const size = string.length;
+    const mod = longestNumber - size;
+    return string[place - mod] || 0;
+}
+
+
+// // unit tests
+// // do not modify the below code
+// describe.skip("radix sort", function () {
+//     it("should sort correctly", () => {
+//         const nums = [
+//             20,
+//             51,
+//             3,
+//             801,
+//             415,
+//             62,
+//             4,
+//             17,
+//             19,
+//             11,
+//             1,
+//             100,
+//             1244,
+//             104,
+//             944,
+//             854,
+//             34,
+//             3000,
+//             3001,
+//             1200,
+//             633
+//         ];
+//         const ans = radixSort(nums);
+//         expect(ans).toEqual([
+//             1,
+//             3,
+//             4,
+//             11,
+//             17,
+//             19,
+//             20,
+//             34,
+//             51,
+//             62,
+//             100,
+//             104,
+//             415,
+//             633,
+//             801,
+//             854,
+//             944,
+//             1200,
+//             1244,
+//             3000,
+//             3001
+//         ]);
+//     });
+//     it("should sort 99 random numbers correctly", () => {
+//         const fill = 99;
+//         const nums = new Array(fill)
+//             .fill()
+//             .map(() => Math.floor(Math.random() * 500000));
+//         const ans = radixSort(nums);
+//         expect(ans).toEqual(nums.sort());
+//     });
+// });
